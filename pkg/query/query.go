1176df03d410eaddf307f6af2bded2b1d7fdb325
Alde Rojas <hello@alde.dev>
2023-09-20 22:00:12 -0500

    Implement improved query client

---
+++ pkg/query/query.go
@@ -0,0 +1,270 @@
+package query
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"sync"
+	"time"
+
+	"github.com/aldehir/ut2u/pkg/encoding/ue2"
+)
+
+type Client struct {
+	conn            *net.UDPConn
+	notifyList      map[string][]chan<- queryResponse
+	notifyListMutex sync.RWMutex
+}
+
+type ServerDetails struct {
+	Info    ServerInfo
+	Rules   []KeyValuePair
+	Players []Player
+}
+
+type Header struct {
+	Version int32
+	Command Command
+}
+
+type ServerInfo struct {
+	ServerID       int32
+	IP             string
+	Port           int32
+	QueryPort      int32
+	ServerName     string
+	MapName        string
+	GameType       string
+	CurrentPlayers int32
+	MaxPlayers     int32
+	Ping           int32
+	Flags          int32
+	SkillLevel     string
+}
+
+type KeyValuePair struct {
+	Key   string
+	Value string
+}
+
+type Player struct {
+	Num     int32
+	Name    string
+	Ping    int32
+	Score   int32
+	StatsID int32
+}
+
+type queryResponse struct {
+	From    net.Addr
+	Header  Header
+	Payload []byte
+}
+
+type Command uint8
+
+const (
+	PingCommand            Command = 0
+	RulesCommmand          Command = 1
+	PlayersCommand         Command = 2
+	RulesAndPlayersCommand Command = 3
+)
+
+const Version = 128
+
+var ErrNoResponse = errors.New("no response")
+var ErrInvalidCommand = errors.New("invalid command")
+
+func NewClient() (*Client, error) {
+	conn, err := net.ListenUDP("udp", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	client := &Client{
+		conn:       conn,
+		notifyList: make(map[string][]chan<- queryResponse),
+	}
+	go client.listen()
+
+	return client, nil
+}
+
+func (c *Client) Close() error {
+	return c.conn.Close()
+}
+
+func (c *Client) Query(ctx context.Context, addr net.Addr) (ServerDetails, error) {
+	responses := make(chan queryResponse, 5)
+	c.notify(addr, responses)
+	c.sendCommand(addr, PingCommand)
+	c.sendCommand(addr, RulesAndPlayersCommand)
+
+	timer := time.NewTimer(250 * time.Millisecond)
+	var details ServerDetails
+	err := ErrNoResponse
+
+loop:
+	for {
+		select {
+		case resp := <-responses:
+			err = enrichDetails(&details, resp)
+			if err != nil {
+				break loop
+			}
+		case <-ctx.Done():
+			err = ctx.Err()
+			break loop
+		case <-timer.C:
+			break loop
+		}
+	}
+
+	timer.Stop()
+	c.stop(addr, responses)
+	close(responses)
+
+	if err != nil {
+		return ServerDetails{}, err
+	}
+
+	return details, nil
+}
+
+func enrichDetails(details *ServerDetails, resp queryResponse) error {
+	switch resp.Header.Command {
+	case PingCommand:
+		return ue2.Unmarshal(resp.Payload, &details.Info)
+	case RulesCommmand:
+		var kv KeyValuePair
+
+		buf := bytes.NewBuffer(resp.Payload)
+		decoder := ue2.NewDecoder(buf)
+
+		for {
+			err := decoder.Decode(&kv)
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				return err
+			}
+
+			details.Rules = append(details.Rules, kv)
+		}
+	case PlayersCommand:
+		var player Player
+
+		buf := bytes.NewBuffer(resp.Payload)
+		decoder := ue2.NewDecoder(buf)
+
+		for {
+			err := decoder.Decode(&player)
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				return err
+			}
+
+			details.Players = append(details.Players, player)
+		}
+	default:
+		return fmt.Errorf("%w: %d", ErrInvalidCommand, resp.Header.Command)
+	}
+
+	return nil
+}
+
+func (c *Client) listen() {
+	packet := make([]byte, 1024)
+
+	for {
+		n, addr, err := c.conn.ReadFrom(packet)
+		if err != nil {
+			if !errors.Is(err, net.ErrClosed) {
+				log.Printf("err: %v", err)
+			}
+			break
+		}
+
+		var header Header
+
+		buf := bytes.NewBuffer(packet[:n])
+		decoder := ue2.NewDecoder(buf)
+
+		err = decoder.Decode(&header)
+		if err != nil {
+			log.Printf("err: could not decode header (%v)", err)
+			continue
+		}
+
+		payload := make([]byte, buf.Len())
+		copy(payload, buf.Bytes())
+		c.dispatch(addr, header, payload)
+	}
+}
+
+func (c *Client) notify(addr net.Addr, ch chan<- queryResponse) {
+	c.notifyListMutex.Lock()
+	defer c.notifyListMutex.Unlock()
+
+	var channels []chan<- queryResponse
+	if list, ok := c.notifyList[addr.String()]; ok {
+		channels = list
+	}
+
+	c.notifyList[addr.String()] = append(channels, ch)
+}
+
+func (c *Client) stop(addr net.Addr, ch chan<- queryResponse) {
+	c.notifyListMutex.Lock()
+	defer c.notifyListMutex.Unlock()
+
+	var channels []chan<- queryResponse
+	if list, ok := c.notifyList[addr.String()]; ok {
+		channels = list
+	}
+
+	for i := len(channels) - 1; i >= 0; i-- {
+		if channels[i] == ch {
+			channels[i] = channels[len(channels)-1]
+			channels = channels[:len(channels)-1]
+		}
+	}
+}
+
+func (c *Client) dispatch(addr net.Addr, header Header, payload []byte) {
+	c.notifyListMutex.RLock()
+	defer c.notifyListMutex.RUnlock()
+
+	if channels, ok := c.notifyList[addr.String()]; ok {
+		for _, ch := range channels {
+			ch <- queryResponse{addr, header, payload}
+		}
+	} else {
+		log.Printf("no channels for %s", addr.String())
+	}
+}
+
+func (c *Client) sendCommand(addr net.Addr, cmd Command) error {
+	payload, err := ue2.Marshal(Header{
+		Version: Version,
+		Command: cmd,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	_, err = c.conn.WriteTo(payload, addr)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ pkg/query/query_test.go
@@ -0,0 +1,27 @@
+package query
+
+import (
+	"context"
+	"net"
+	"testing"
+)
+
+func TestClientQuery(t *testing.T) {
+	client, err := NewClient()
+	if err != nil {
+		t.Error(err)
+	}
+	defer client.Close()
+
+	addr, err := net.ResolveUDPAddr("udp", "nj-1.kokuei.dev:7778")
+	if err != nil {
+		t.Error(err)
+	}
+
+	details, err := client.Query(context.TODO(), addr)
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Logf("%+v", details)
+}
